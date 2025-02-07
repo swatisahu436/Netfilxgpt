a2aca7de42b78ede4434f98a013f34b66f9c28ff
Swatisahu01122000 <swatisahu01122000@gmail.com>
2025-02-07 21:04:58 +0530

    netflix gpt

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='text-xl font-bold text-green-600 bg-red-500'>
+      
+      </div>
+    </>
+  )
+}
+
+export default App;
